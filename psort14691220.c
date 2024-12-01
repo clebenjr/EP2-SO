de24acd334cee80eac7d48564047c96614a68d5e
clebenjr <clebenjuniorcgarcia@hotmail.com>
2024-12-01 20:58:42 -0300

    codigo aparentemente funcionando com threads

--- psort14691220.c
+++ psort14691220.c
@@ -3,6 +3,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
 
 #define TAM_REGISTRO 100
 #define TAM_CHAVE 4
@@ -13,48 +14,78 @@ typedef struct {
     char dados[TAM_DADOS];
 } Registro;
 
+typedef struct {
+    int inicio;
+    int fim;
+} ThreadArgs;
+
 Registro *registros;
 int qnt_registros;
+int num_threads;
 
-void merge(Registro *arr, int inicio, int meio, int fim) {
+void merge(int inicio, int meio, int fim);
+void* thread_merge_sort(void* arg);
+void merge_sort(int inicio, int fim);
+
+void merge(int inicio, int meio, int fim) {
     int n1 = meio - inicio + 1;
     int n2 = fim - meio;
 
     Registro *esq = (Registro *)malloc(n1 * sizeof(Registro));
     Registro *dir = (Registro *)malloc(n2 * sizeof(Registro));
 
-    for (int i = 0; i < n1; i++)
-        esq[i] = arr[inicio + i];
-    for (int i = 0; i < n2; i++)
-        dir[i] = arr[meio + 1 + i];
+    int i;
 
-    int i = 0, j = 0, k = inicio;
+    for (i = 0; i < n1; i++)
+        esq[i] = registros[inicio + i];
+    for (i = 0; i < n2; i++)
+        dir[i] = registros[meio + 1 + i];
+
+    i = 0;
+    int j = 0;
+    int k = inicio;
 
     while (i < n1 && j < n2) {
         if (esq[i].chave <= dir[j].chave)
-            arr[k++] = esq[i++];
+            registros[k++] = esq[i++];
         else
-            arr[k++] = dir[j++];
+            registros[k++] = dir[j++];
     }
 
     while (i < n1)
-        arr[k++] = esq[i++];
+        registros[k++] = esq[i++];
 
     while (j < n2)
-        arr[k++] = dir[j++];
+        registros[k++] = dir[j++];
 
     free(esq);
     free(dir);
 }
 
-void merge_sort(Registro *arr, int inicio, int fim) {
+void* thread_merge_sort(void* arg) {
+    ThreadArgs* args = (ThreadArgs*)arg;
+    int inicio = args->inicio;
+    int fim = args->fim;
+
     if (inicio < fim) {
         int meio = inicio + (fim - inicio) / 2;
 
-        merge_sort(arr, inicio, meio);
-        merge_sort(arr, meio + 1, fim);
+        merge_sort(inicio, meio);
+        merge_sort(meio + 1, fim);
 
-        merge(arr, inicio, meio, fim);
+        merge(inicio, meio, fim);
+    }
+    return NULL;
+}
+
+void merge_sort(int inicio, int fim) {
+    if (inicio < fim) {
+        int meio = inicio + (fim - inicio) / 2;
+
+        merge_sort(inicio, meio);
+        merge_sort(meio + 1, fim);
+
+        merge(inicio, meio, fim);
     }
 }
 
@@ -85,7 +116,40 @@ int main(int argc, char* argv[]) {
     }
     fclose(entrada);
 
-    merge_sort(registros, 0, qnt_registros - 1);
+    num_threads = atoi(argv[3]);
+    if (num_threads <= 0) {
+        num_threads = sysconf(_SC_NPROCESSORS_ONLN);
+    }
+
+    pthread_t threads[num_threads];
+    ThreadArgs args[num_threads];
+    int tamanho_por_thread = qnt_registros / num_threads;
+    int i; 
+
+    for (i = 0; i < num_threads; i++) {
+        args[i].inicio = i * tamanho_por_thread;
+        if (i != num_threads - 1) {
+            args[i].fim = (i + 1) * tamanho_por_thread - 1;
+        } else {
+            args[i].fim = qnt_registros - 1;
+        }
+        pthread_create(&threads[i], NULL, thread_merge_sort, &args[i]);
+    }
+
+    for (i = 0; i < num_threads; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    for (i = 1; i < num_threads; i++) {
+        int meio = (i * tamanho_por_thread) - 1;
+        int fim;
+        if (i != num_threads - 1) {
+            fim = (i + 1) * tamanho_por_thread - 1;
+        } else {
+            fim = qnt_registros - 1;
+        }
+        merge(0, meio, fim);
+    }
 
     int fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
     if (fd == -1) {
